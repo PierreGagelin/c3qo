2ec8a09382dd6f5f72a034151e25d74edfbd7bdc
Pierre Gagelin <pierre.gagelin@gmail.com>
2018-01-04 21:35:31 +0100

    Added basic CLI

--- c3qo/engine/src/main.c
+++ c3qo/engine/src/main.c
@@ -1,23 +1,49 @@
 
 
 #include <stdlib.h>
+#include <unistd.h> /* getopt */
 
 #include "c3qo/logger.h"
 #include "c3qo/manager.h"
 
 
+extern char *optarg;
+
+
 int main(char argc, char ** argv)
 {
-        bool conf;
-        int  ret = 0;
-
-        (void) argc;
-        (void) argv;
+        bool       conf;
+        int        opt;
+        const char *filename = "/tmp/config.txt";
+        int        ret = 0;
 
         LOGGER_OPEN();
 
+        while ((opt = getopt(argc, argv, "hf:")) != -1)
+        {
+                switch (opt)
+                {
+                case 'h':
+                {
+                        LOGGER_DEBUG("lol, help is for the weaks");
+                        break;
+                }
+                case 'f':
+                {
+                        LOGGER_DEBUG("CLI file to load configuration : %s", optarg);
+                        filename = optarg;
+                        break;
+                }
+                default:
+                {
+                        LOGGER_ERR("CLI argument error");
+                        break;
+                }
+                }
+        }
+
         /* Parse configuration file */
-        conf = manager_conf_parse("/tmp/config.txt");
+        conf = manager_conf_parse(filename);
         if (conf == false)
         {
                 ret = -1;
