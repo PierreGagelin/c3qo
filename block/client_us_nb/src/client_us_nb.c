0522ecd8ef4fad317250b01ef1ee3e48cd0bb180
Pierre Gagelin <pierre.gagelin@gmail.com>
2018-01-21 21:18:40 +0100

    Separated connection code as it will be a callback for timer signal

--- block/client_us_nb/src/client_us_nb.c
+++ block/client_us_nb/src/client_us_nb.c
@@ -8,6 +8,7 @@
 
 
 #include <unistd.h>     /* close */
+#include <stdio.h>      /* snprintf */
 #include <string.h>     /* memset */
 #include <sys/types.h>  /* getsockopt */
 #include <sys/un.h>     /* sockaddr_un */
@@ -32,6 +33,9 @@ struct client_us_nb_ctx
 struct client_us_nb_ctx ctx_c;
 
 
+/**
+ * @brief Remove a managed file descriptor and close it
+ */
 static inline void client_us_nb_clean()
 {
         manager_fd_remove(ctx_c.fd, true);
@@ -52,14 +56,14 @@ static void client_us_nb_callback(int fd)
                 return;
         }
 
-        LOGGER_DEBUG("Received data on socket [fd=%d]", fd);
+        LOGGER_DEBUG("Received data on socket. Not implemented yet [fd=%d]", fd);
 }
 
 
 /**
- * @brief Callback function to connect socket
+ * @brief Check that the socket is connected
  */
-static void client_us_nb_connect(int fd)
+static void client_us_nb_connect_check(int fd)
 {
         socklen_t          lon;
         int                optval;
@@ -92,7 +96,31 @@ static void client_us_nb_connect(int fd)
 
 
 /**
- * @brief Initialization function
+ * @brief Connect to a server
+ */
+static int client_us_nb_connect(int fd)
+{
+        struct sockaddr_un clt_addr;
+        int                ret;
+
+        memset(&clt_addr, 0, sizeof(clt_addr));
+        clt_addr.sun_family = AF_UNIX;
+        ret = snprintf(clt_addr.sun_path, sizeof(clt_addr.sun_path), SOCKET_NAME);
+        if (ret < 0)
+        {
+                LOGGER_ERR("Failed snprintf [buf=%p ; size=%lu ; string=%s]", clt_addr.sun_path, sizeof(clt_addr.sun_path), SOCKET_NAME);
+        }
+        else if (((size_t) ret) > sizeof(clt_addr.sun_path))
+        {
+                LOGGER_ERR("Failed to write socket name, it's too large [sun_path=%s ; max_size=%lu]", SOCKET_NAME, sizeof(clt_addr.sun_path));
+        }
+
+        return c3qo_socket_connect_nb(fd, (struct sockaddr *) &clt_addr, sizeof(clt_addr));
+}
+
+
+/**
+ * @brief Initialize the block
  */
 static void client_us_nb_init()
 {
@@ -109,8 +137,7 @@ static void client_us_nb_init()
  */
 static void client_us_nb_start()
 {
-        struct sockaddr_un clt_addr;
-        int                ret;
+        int ret;
 
         LOGGER_INFO("Start block client_us_nb");
 
@@ -126,10 +153,7 @@ static void client_us_nb_start()
         c3qo_socket_set_nb(ctx_c.fd);
 
         /* Connect the socket to the server */
-        memset(&clt_addr, 0, sizeof(clt_addr));
-        clt_addr.sun_family = AF_UNIX;
-        strcpy(clt_addr.sun_path, SOCKET_NAME);
-        ret = c3qo_socket_connect_nb(ctx_c.fd, (struct sockaddr *) &clt_addr, sizeof(clt_addr));
+        ret = client_us_nb_connect(ctx_c.fd);
         if (ret == -1)
         {
                 LOGGER_ERR("Failed to connect to server [fd=%d]", ctx_c.fd);
@@ -139,7 +163,7 @@ static void client_us_nb_start()
         else if (ret == 1)
         {
                 LOGGER_DEBUG("Server not ready to receive client socket, will try later [fd=%d]", ctx_c.fd);
-                manager_fd_add(ctx_c.fd, &client_us_nb_connect, false);
+                manager_fd_add(ctx_c.fd, &client_us_nb_connect_check, false);
                 return;
         }
         else if (ret == 2)
@@ -150,7 +174,7 @@ static void client_us_nb_start()
         }
         else
         {
-                /* Success : register the file descriptor with a callback for data reception */
+                /* Success: register the file descriptor with a callback for data reception */
                 if (manager_fd_add(ctx_c.fd, &client_us_nb_callback, true) == false)
                 {
                         LOGGER_ERR("Failed to register callback on client socket [fd=%d ; callback=%p]", ctx_c.fd, &client_us_nb_callback);
@@ -204,7 +228,7 @@ static void client_us_nb_ctrl(enum bk_cmd cmd, void *arg)
         }
         default:
         {
-                LOGGER_ERR("Unknown bk_cmd=%d called", cmd);
+                LOGGER_ERR("Unknown command called [bk_cmd=%d]", cmd);
                 break;
         }
         }
