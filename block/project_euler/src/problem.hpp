390ac4acc66e3f3e6065cf3275bb6e66a66023ad
Pierre Gagelin <pierre.gagelin@gmail.com>
2018-05-06 09:29:51 +0200

    Added problem 2, begin of 51; rework to be dynamic

--- block/project_euler/src/problem.hpp
+++ block/project_euler/src/problem.hpp
@@ -2,13 +2,17 @@
 
 // C++ library headers
 #include <cerrno>
+#include <cmath>
 #include <cstdlib>
-
-extern "C" {
-#include <string.h>
-}
+#include <cstring>
+#include <vector>
 
 // Project headers
 #include "utils/logger.hpp"
 
+// We need those to have a demangled name to be loaded at run-time
+extern "C" {
 void solve_problem_1(char *param);
+void solve_problem_2(char *param);
+void solve_problem_51(char *param);
+}
