c240547f3474f881a1eea139c8b5283ba4614ef6
Pierre Gagelin <pierre.gagelin@gmail.com>
2018-07-26 15:11:45 +0200

    Forgot a file

---
+++ utils/include/utils/include.hpp
@@ -0,0 +1,28 @@
+// Useful includes
+
+// C headers
+extern "C"
+{
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <syslog.h>
+#include <unistd.h>
+#include <zmq.h>
+}
+
+// C++ headers
+#include <cerrno>
+#include <cmath>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <forward_list>
+#include <fstream>
+#include <sstream>
+#include <tuple>
+#include <unordered_map>
+#include <vector>
